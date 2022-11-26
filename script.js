128effcf4186780d3f422cad7492abf8dd85cae6
Daniil Zavyalov <dezavv1@gmail.com>
2022-11-26 19:27:32 +0600

    Move css and javascript to separate files

---
+++ script.js
@@ -0,0 +1,12 @@
+function photoClickAlert() {
+    alert("Please don't click here");
+}
+function mouseEnterHandler(event) {
+    console.log("event", event);
+}
+function mouseOutHandler(event) {
+    console.log("event", event);
+}
+function changeInputAlert(event) {
+    alert(event.target.value);
+}
